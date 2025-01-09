e32a5998218ef3706cb86ce36aaa2a400f04c7b0
azharkhattak123 <azharmehmoo33@gmail.com>
2025-01-09 20:43:49 +0500

    adding some new thing

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,9 +6,13 @@ import bell_icon from "../../assets/bell_icon.svg";
 import profile_img from "../../assets/profile_img.png";
 import caret_icon from "../../assets/caret_icon.svg";
 import { logout } from "../../firebase";
+import hamberger from "../../assets/hamberger.svg";
+import times from "../../assets/times.svg";
 
 const Navbar = () => {
   const navRef = useRef();
+  const mobNavRef = useRef();
+
   useEffect(() => {
     window.addEventListener("scroll", () => {
       if (window.scrollY >= 140) {
@@ -19,32 +23,34 @@ const Navbar = () => {
     });
   }, []);
   return (
-    <div ref={navRef} className="navbar">
-      <div className="navbar_left">
-        <img src={logo} alt="" />
-        <ul>
-          <li>Home</li>
-          <li>TV Show</li>
-          <li>Movies</li>
-          <li>New and Popular</li>
-          <li>My list</li>
-          <li>Browse by language</li>
-        </ul>
-      </div>
-      <div className="navbar_right">
-        <img src={search_icon} alt="" className="search" />
-        <p>Children</p>
-        <img src={bell_icon} alt="" className="search" />
+    <>
+      <div ref={navRef} className="navbar">
+        <div className="navbar_left">
+          <img src={logo} alt="" />
+          <ul>
+            <li>Home</li>
+            <li>TV Show</li>
+            <li>Movies</li>
+            <li>New and Popular</li>
+            <li>My list</li>
+            <li>Browse by language</li>
+          </ul>
+        </div>
+        <div className="navbar_right">
+          <img src={search_icon} alt="" className="search" />
+          <p>Children</p>
+          <img src={bell_icon} alt="" className="search" />
 
-        <div className="navbar_profile">
-          <img src={profile_img} alt="" className="profile_img" />
-          <img src={caret_icon} alt="" />
-          <div className="dropdown">
-            <p onClick={logout}>Log Out from Netflix</p>
+          <div className="navbar_profile">
+            <img src={profile_img} alt="" className="profile_img" />
+            <img src={caret_icon} alt="" />
+            <div className="dropdown">
+              <p onClick={logout}>Log Out from Netflix</p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
